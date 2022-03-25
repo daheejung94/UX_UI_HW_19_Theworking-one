d1ba7be8d81f725554f5414d97bd1e7adfa3c764
daheejung94 <daheejung94@gmail.com>
2022-03-25 16:43:07 -0700

    this should work v1

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).on("click", "#resume", function() 
+{
+  $("#modal").css("display", "block");
+}
+);
+
+$(document).on("click", ".close", function()
+{
+  $("#modal").css("display", "none");
+})
+
+
